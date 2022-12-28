b1e607564f1513463cadf3ad7d28e51068f45ab8
Dmitriy <dimazurenko@gmail.com>
2022-12-28 13:28:29 +0300

    add Demo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toogleParagraphHandler}>toogle paragraph</Button>
     </div>
   );
