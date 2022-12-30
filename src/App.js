9de7ba78a1645c34bb4b3a2a780052eeea869961
Dmitriy <dimazurenko@gmail.com>
2022-12-30 12:50:19 +0300

    init new project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 
+import MoviesList from './components/MoviesList';
 import './App.css';
-import DemoOutput from './components/Demo/DemoOutput';
-import Button from './components/UI/Button/Button';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-
-  const toogleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []);
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
 
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={toogleParagraphHandler}>toogle paragraph</Button>
-    </div>
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
   );
 }
 
