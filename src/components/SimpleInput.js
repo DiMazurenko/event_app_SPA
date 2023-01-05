9248a0782c12790675940c8d208c64c53b7881e6
Dmitriy <dimazurenko@gmail.com>
2023-01-05 18:25:18 +0300

    fix validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -79,7 +79,7 @@ const SimpleInput = (props) => {
       <div className={emailInputClasses}>
         <label htmlFor="email">Your Email</label>
         <input
-          type="text"
+          type="email"
           id="email"
           onChange={emailInputChangeHandler}
           onBlur={emailInputBlurHandler}
