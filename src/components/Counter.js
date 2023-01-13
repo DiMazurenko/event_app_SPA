be9d5ea8e0388cbd24f01935d31946dab0ab5d36
Dmitriy <dimazurenko@gmail.com>
2023-01-13 15:16:44 +0300

    add handlers

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,22 +5,37 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
   };
+
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   };
 
-  const toggleCounterHandler = () => {};
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 5 });
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toogle' });
+  };
+
+  const contentCounter = (
+    <>
+      <div className={classes.value}>{counter}</div>
+    </>
+  );
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && contentCounter}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
