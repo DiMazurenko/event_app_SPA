278a46ddb4c91e20b98ef5dee9d0ca51b9ba7541
Dmitriy <dimazurenko@gmail.com>
2023-01-28 15:33:27 +0300

    add Products and link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from './pages/Home';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // createRoutesFromElements,
+  // Route,
+} from 'react-router-dom';
 
-const router = createBrowserRouter([{ path: '/', element: <HomePage /> }, {}]);
+import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
+
+// const routeDefinition = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+// const router = createBrowserRouter(routeDefinition);
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/products', element: <ProductsPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
