b4e394050afd2feebe445c24a8461806f4b72d01
Dmitriy <dimazurenko@gmail.com>
2023-02-18 15:17:56 +0300

    rever commits

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -33,9 +33,7 @@ function EventDetailPage() {
 export default EventDetailPage;
 
 async function loadEvent(id) {
-  const response = await fetch(
-    'https://event-app-bqillih53-dimazurenko.vercel.app/events/' + id
-  );
+  const response = await fetch('http://localhost:8080/events/' + id);
 
   if (!response.ok) {
     throw json(
@@ -51,9 +49,7 @@ async function loadEvent(id) {
 }
 
 async function loadEvents() {
-  const response = await fetch(
-    'https://event-app-bqillih53-dimazurenko.vercel.app/events'
-  );
+  const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     // return { isError: true, message: 'Could not fetch events.' };
@@ -85,15 +81,12 @@ export async function action({ params, request }) {
   const eventId = params.eventId;
   const token = getAuthToken();
 
-  const response = await fetch(
-    'https://event-app-bqillih53-dimazurenko.vercel.app/events/' + eventId,
-    {
-      method: request.method,
-      headers: {
-        Authorization: 'Bearer ' + token,
-      },
-    }
-  );
+  const response = await fetch('http://localhost:8080/events/' + eventId, {
+    method: request.method,
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+  });
 
   if (!response.ok) {
     throw json(
